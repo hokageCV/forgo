b6b034a3cb54d2859cd77001ae039ad6f31e29bc
hokageCV <chaitanyavaru@gmail.com>
2023-07-24 19:03:11 +0530

    add task list box

--- frontend/src/components/TaskList.tsx
+++ frontend/src/components/TaskList.tsx
@@ -8,7 +8,7 @@ type TaskListProps = {
 
 export default function TaskList({ taskList }: TaskListProps) {
     return (
-        <div class="flex flex-col justify-center items-center bg-frost-1 rounded-md">
+        <div class="flex flex-col justify-center items-center bg-frost-1 rounded-md p-4 m-5">
             {taskList.length > 0 ? (
                 taskList.map((task) => <Task key={task.id} task={task} />)
             ) : (
